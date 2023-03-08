614a72c1e45c0e528112b243bf70b141a7d6310b
Minh Bao <bebaoboy@gmail.com>
2023-03-08 09:36:25 +0700

    change path to relative

--- inclass_w5/Relative2AbsoluteConverter.cs
+++ inclass_w5/Relative2AbsoluteConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace bookManager
+namespace inclass_w5
 {
     internal class Relative2AbsoluteConverter : IValueConverter
     {
