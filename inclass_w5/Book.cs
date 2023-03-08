a19e9eeb60d81c00ebb882c62c71fc88d7c80e70
Minh Bao <bebaoboy@gmail.com>
2023-03-08 09:25:41 +0700

    Add project files.

---
+++ inclass_w5/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inclass_w5 { 
+    public class Book :INotifyPropertyChanged
+    {
+        private string _title;
+        public string title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(title)));
+
+            }
+        }
+        private string _coverImage;
+        public string coverImage
+        {
+            get
+            {
+                return _coverImage;
+            }
+            set
+            {
+                _coverImage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(coverImage)));
+
+            }
+        }
+        private string _author;
+        public string author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(author)));
+
+            }
+        }
+        private int _publishedYear;
+        public int publishedYear
+        {
+            get { return _publishedYear; }
+            set
+            {
+                _publishedYear = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(author)));
+
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+    }
+}
---
+++ inclass_w5/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace inclass_w5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<Book> books = null;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            books = new ObservableCollection<Book>();
+            foreach (var line in File.ReadAllLines("data/book_data.csv")) {
+                var element = line.Split(",");
+                books.Add(new Book()
+                {
+                    coverImage = element[0],
+                    title = element[1],
+                    publishedYear = int.Parse(element[2]),
+                    author = element[3]
+                });
+            }
+            booksComboBox.ItemsSource = books;
+            booksComboBox.SelectedIndex = 0;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            books.Add(new Book()
+            {
+                title = "Nha gia kim",
+                coverImage = "img/nhagiakim.jpg",
+                publishedYear = 1988,
+                author = "Paulo Coelho"
+            });
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            int i = booksComboBox.SelectedIndex;
+            books.RemoveAt(i);
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int i = booksComboBox.SelectedIndex;
+            books[i].title = "Nha Gia Kim";
+            books[i].author = "Paulo Coelho";
+            books[i].coverImage = "img/nhagiakim.jpg";
+            books[i].publishedYear = 1988;
+        }
+    }
+}
